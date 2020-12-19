e82dd14a3dc91f1638725a206b73465b6c41336f
chenxunhan <chenxunhan@foxmail.com>
2020-12-19 11:28:15 +0800

    add for(j)
    add for(j)

--- MyFirstProject/firstCpp.cpp
+++ MyFirstProject/firstCpp.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
   int i;
   for (i = 0; i < 100; ++i) {
-    cout << i << endl;
+        for(int j=0;j<100;++j)
+            cout << i << endl;
     //i edit code here
   }
   return 0;
